c759b8263750a39ac9c3303248466bb0f7657aee
Ali Afsahnoudeh <ali.afsah1988@gmail.com>
2021-01-18 20:47:02 +0100

    auth middleware injection

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,11 @@ import Logger from './services/Logger';
 import DatabaseManager from './database/manager/DatabaseManager';
 import SampleValidator from './api/validators/SampleValidator';
 import GroupRepository from './database/repositories/GroupRepository';
+import UserRepository from './database/repositories/UserRepository';
 import GroupController from './api/controllers/GroupController';
 import GroupRoutes from './api/routes/GroupRoutes';
 import DateHelper from './helpers/DateHelper';
+import AuthMiddleware from './api/middleware/AuthMiddleware';
 
 import IocContainer from './services/IocContainer';
 const iocContainer = new IocContainer();
@@ -25,8 +27,10 @@ iocContainer.service('DatabaseManager', c => DatabaseManager.getInstance(
 iocContainer.service('SampleValidator', c => new SampleValidator(c.Logger));
 iocContainer.service('DateHelper', c => new DateHelper(c.Logger));
 iocContainer.service('GroupRepository', c => new GroupRepository(c.DatabaseManager, c.Logger));
-iocContainer.service('SampleController', c => new GroupController(c.GroupRepository, c.SampleValidator, c.Logger, c.DateHelper));
-iocContainer.service('SampleRoutes', c => new GroupRoutes(c.SampleController, c.Logger));
+iocContainer.service('UserRepository', c => new UserRepository(c.DatabaseManager, c.Logger));
+iocContainer.service('GroupController', c => new GroupController(c.GroupRepository, c.SampleValidator, c.Logger, c.DateHelper));
+iocContainer.service('AuthMiddleware', c => new AuthMiddleware(c.Logger, c.UserRepository ));
+iocContainer.service('SampleRoutes', c => new GroupRoutes(c.GroupController, c.Logger, c.AuthMiddleware));
 iocContainer.service('RestServer', c => new RestServer(config.SERVER_PORT, config.SERVER_HOST, c.SampleRoutes, c.Logger));
 
 iocContainer.get('DatabaseManager').connect();
